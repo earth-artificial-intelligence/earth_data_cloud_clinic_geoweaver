0ca6b6e06b1976b4182b4d2bd8e8108f663e05f8
Gokul Prathin <gokulprathin8@gmail.com>
2023-06-14 12:32:12 -0400

    added package install

--- code/earth_access_authenticate.py
+++ code/earth_access_authenticate.py
@@ -1,5 +1,13 @@
 from search_data import *
 
+
+ssh_short_name = "SEA_SURFACE_HEIGHT_ALT_GRIDS_L4_2SATS_5DAY_6THDEG_V_JPL2205"
+
+results = earthaccess.search_data(
+    short_name=ssh_short_name,
+    cloud_hosted=True,
+    temporal=("2021-07-01", "2021-09-30"),
+)
 sst_short_name="MUR-JPL-L4-GLOB-v4.1"
 
 results = earthaccess.search_data(
